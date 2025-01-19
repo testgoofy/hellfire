174ecf71cb8a08d897a2c6f797a61ef6fcf3d245
Lukas Gysin <L.Gysin@pm.me>
2025-01-19 10:37:44 +0100

    feat: Construction Manager

---
+++ src/managers/constructionManager.ts
@@ -0,0 +1,51 @@
+var _ = require('lodash');
+
+export default class ConstructionManager {
+  private static instance: ConstructionManager;
+
+  /// Map of construction sites id and the remaining progress needed
+  private constructionSites = new Map<string, number>();
+
+  public static getInstance(room: Room): ConstructionManager {
+    if (!ConstructionManager.instance) {
+      ConstructionManager.instance = new ConstructionManager(room);
+    }
+
+    return ConstructionManager.instance;
+  }
+
+  private constructor(room: Room) {
+    let constructions = room.find(FIND_MY_CONSTRUCTION_SITES);
+
+    for (let construction of constructions) {
+      this.constructionSites.set(
+        construction.id,
+        construction.progressTotal - construction.progress
+      );
+    }
+  }
+
+  public registerWork(amount: number, id: string): ConstructionSite | null;
+  public registerWork(amount: number, id?: string): ConstructionSite | null {
+    if (id) {
+      // Register Work on a specific construction site
+      let remainingAmount = this.constructionSites.get(id);
+      if (remainingAmount && remainingAmount > 0) {
+        this.constructionSites.set(id, remainingAmount - amount);
+        return Game.getObjectById<ConstructionSite>(id);
+      }
+      return null;
+    } else {
+      // Choose a construction site
+      for (let [id, remainingAmount] of this.constructionSites.entries()) {
+        if (remainingAmount > 0) {
+          this.constructionSites.set(id, remainingAmount - amount);
+          return Game.getObjectById<ConstructionSite>(id);
+        }
+      }
+    }
+
+    // No construction site found
+    return null;
+  }
+}
